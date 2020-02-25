127c11b80703c4b527d0194bff64da870947a2df
Pandey <pandvike@38f9d3698f00.ant.amazon.com>
2020-02-25 18:03:35 +0100

    partially modified json str

---
+++ scripts/rekognition_test.py
@@ -0,0 +1,33 @@
+import json
+
+
+
+
+
+new_value = {'image_size': [{'width': 960, 'height': 540, 'depth': 3}]}
+
+
+def edit_json():
+
+    with open("formatted_json.json", "r") as read_file:
+        data = json.load(read_file)
+        for item in data.values():
+            item['bounding-box'] = item.pop('size')
+            item['bounding-box'] = new_value
+            item['source-ref'] = "s3 bucket location"
+            for region in item['regions'].values():
+                shape_attributes = region['shape_attributes']
+                shape_attributes['class_id'] = shape_attributes.pop('name')
+                shape_attributes['left'] = shape_attributes.pop('x')
+                shape_attributes['top'] = shape_attributes.pop('y')
+
+            item['annotations'] = item.pop('regions')
+
+    with open('new.json', 'w') as outfile:
+        print("data is there: ", data)
+        json.dump(data, outfile, indent=2)
+
+
+if __name__ == '__main__':
+    data = edit_json()
+
