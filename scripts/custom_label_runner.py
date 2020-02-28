b1cfb77acbce45888f518fd02ef67032a1944912
Pandey <pandvike@38f9d3698f00.ant.amazon.com>
2020-02-29 00:35:34 +0100

    refactoring

---
+++ scripts/custom_label_runner.py
@@ -0,0 +1,59 @@
+import json
+
+import boto3
+
+
+def create_project(project_name):
+    client = boto3.client('rekognition')
+
+    # Create a project
+    print('Creating project:' + project_name)
+    response = client.create_project(ProjectName=project_name)
+    project_arn = response['ProjectArn']
+    print('project created, ARN is: ' + project_arn)
+    return project_arn
+
+
+def train_model(project_arn, version_name, output_config, training_dataset, testing_dataset):
+    client = boto3.client('rekognition')
+    print('starting training version no: ' + version_name)
+
+    try:
+        response = client.create_project_version(
+            ProjectArn=project_arn,
+            VersionName=version_name,
+            OutputConfig=output_config,
+            TrainingData=training_dataset,
+            TestingData=testing_dataset)
+
+        project_version_training_completed_waiter = client.get_waiter('project_version_training_completed')
+        project_version_training_completed_waiter.wait(ProjectArn=project_arn, VersionNames=[version_name])
+
+        describe_response = client.describe_project_versions(ProjectArn=project_arn, VersionNames=[version_name])
+        for model in describe_response['ProjectVersionDescriptions']:
+            print("Status: " + model['Status'])
+            print("Message: " + model['StatusMessage'])
+    except Exception as e:
+        print(e)
+
+    print('Training completed...')
+
+
+def main():
+    # project_name = 'custom-labels-05'
+    # project_arn = create_project(project_name)
+    project_arn = 'arn:aws:rekognition:us-east-1:018632230441:project/custom-labels-05/1582930535021'
+    version_name = '1.0'
+
+    output_config = json.loads('{"S3Bucket": "pandvike-custom-label-output", "S3KeyPrefix": "2802"}')
+    training_dataset = json.loads(
+        '{"Assets": [{ "GroundTruthManifest": { "S3Object": { "Bucket": "pandvike-custom-label-training-dataset", "Name": "training_dataset.manifest" } } } ] }')
+    testing_dataset = json.loads('{"AutoCreate":true}')
+    testing_dataset = json.loads(
+        '{"Assets": [{ "GroundTruthManifest": { "S3Object": { "Bucket": "pandvike-custom-label-testing-dataset", "Name": "testing_dataset.manifest" } } } ]}')
+
+    train_model(project_arn, version_name, output_config, training_dataset, testing_dataset)
+
+
+if __name__ == "__main__":
+    main()
--- scripts/rekognition_test.py
+++ scripts/rekognition_test.py
@@ -4,30 +4,47 @@ import json
 
 
 
-img_dimensions = {'image_size': [{'width': 960, 'height': 540, 'depth': 3}]}
+# img_dimensions = [{'width': 960, 'height': 540, 'depth': 3}]
 
 
-def edit_json():
+# def edit_json():
+#
+#     with open("training_dataset.manifest", "r") as read_file:
+#         data = json.load(read_file)
+#         for item in data.values():
+#             item['image_size'] = item.pop('size')
+#             item['image_size'] = img_dimensions
+#             # item['bounding-box'] = img_dimensions
+#             item['source-ref'] = "s3 bucket location"
+#             for region in item['regions']:
+#                 region['class_id'] = region.pop('name')
+#                 region['left'] = region.pop('x')
+#                 region['top'] = region.pop('y')
+#
+#             item['annotations'] = item.pop('regions')
+#
+#     with open('new.json', 'w') as outfile:
+#         #print("data is there: ", data)
+#         json.dump(data, outfile, indent=2)
+#
+#     #print(json.dumps(data))
 
-    with open("formatted_json.json", "r") as read_file:
-        data = json.load(read_file)
-        for item in data.values():
-            item['bounding-box'] = item.pop('size')
-            item['bounding-box'] = img_dimensions
-            item['source-ref'] = "s3 bucket location"
-            for region in item['regions'].values():
-                shape_attributes = region['shape_attributes']
-                shape_attributes['class_id'] = shape_attributes.pop('name')
-                shape_attributes['left'] = shape_attributes.pop('x')
-                shape_attributes['top'] = shape_attributes.pop('y')
 
-            item['annotations'] = item.pop('regions')
+# import boto3
+#
+# def create_project(project_name):
+#
+#     client=boto3.client('rekognition')
+#
+#     #Create a project
+#     print('Creating project:' + project_name)
+#     response=client.describe_projects()
+#     print(response)
+#     print('Done...')
+#
+# def main():
+#     project_name='project'
+#     create_project(project_name)
 
-    with open('new.json', 'w') as outfile:
-        print("data is there: ", data)
-        json.dump(data, outfile, indent=2)
-
-
-if __name__ == '__main__':
-    data = edit_json()
-
+# if __name__ == "__main__":
+#     edit_json()
